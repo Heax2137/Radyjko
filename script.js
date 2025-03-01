97ebb4e539d6b93b0bc1c6e0876955ee46c4bd6d
Heax2137 <dominikbomba12@gmail.com>
2025-03-01 06:21:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Tablica z linkami do piosenek
     const songs = [
         "https://www.dropbox.com/s/xyz123/piosenka1.mp3?raw=1",
-        "https://www.dropbox.com/scl/fi/w5rwwvop1nucodd7tr9tw/piosenka2.mp3?rlkey=wabypyg1dc3nvuogtuvz2z948&st=5pmahm9s&raw=1",
-        ""https://www.dropbox.com/scl/fi/wem6fev8hkupvswtam328/piosenka3.mp3?rlkey=wr0z0rj8opt93bkizfzhwotoc&st=y86q29gm&raw=1"
+        "https://www.dropbox.com/s/xyz123/piosenka2.mp3?raw=1",
+        "https://www.dropbox.com/s/xyz123/piosenka3.mp3?raw=1"
     ];
 
     // Pobierz element audio
