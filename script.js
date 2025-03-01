1914eb12c6648c279f147cf602e50ae71b249c8e
Heax2137 <dominikbomba12@gmail.com>
2025-03-01 05:32:48 +0100

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const songs = [
-    "https://github.com/Heax2137/Radyjko/blob/0f90105b13e8800aeafd5f40f16377121706027d/Radio%20Chlew%20-%20Alie%20Olie.mp3",
+    "https://github.com/Heax2137/Radyjko/raw/refs/heads/main/Radio%20Chlew%20-%20Alie%20Olie.mp3",
     "https://github.com/Heax2137/Radyjko/blob/0f90105b13e8800aeafd5f40f16377121706027d/Radio%20Chlew%20-%20Alie%20Olie.mp3",
     "https://github.com/Heax2137/Radyjko/blob/0f90105b13e8800aeafd5f40f16377121706027d/Radio%20Chlew%20-%20Alie%20Olie.mp3"
 ];
