2fd12c997f4a4ba5146ac5bd8e35e70f0118a7ab
Heax2137 <dominikbomba12@gmail.com>
2025-03-01 06:20:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Tablica z linkami do piosenek
     const songs = [
-        "",
-        "https://www.dropbox.com/scl/fi/w5rwwvop1nucodd7tr9tw/piosenka2.mp3?rlkey=wabypyg1dc3nvuogtuvz2z948&st=5pmahm9s&raw=1",
-        "https://www.dropbox.com/scl/fi/wem6fev8hkupvswtam328/piosenka3.mp3?rlkey=wr0z0rj8opt93bkizfzhwotoc&st=y86q29gm&raw=1"
+        "https://www.dropbox.com/s/xyz123/piosenka1.mp3?raw=1",
+        "https://www.dropbox.com/s/xyz123/piosenka2.mp3?raw=1",
+        "https://www.dropbox.com/s/xyz123/piosenka3.mp3?raw=1"
     ];
 
     // Pobierz element audio
     const audioPlayer = document.getElementById('player');
+    const audioSource = document.getElementById('audioSource');
 
     // Funkcja zmieniająca piosenki po zakończeniu aktualnej
     function changeSong() {
         // Losuj nową piosenkę
         const randomSong = songs[Math.floor(Math.random() * songs.length)];
-        audioPlayer.src = randomSong;
+        audioSource.src = randomSong;  // Zmień źródło na nową piosenkę
+        audioPlayer.load(); // Załaduj nową piosenkę
         audioPlayer.play(); // Uruchom nową piosenkę
     }
 
