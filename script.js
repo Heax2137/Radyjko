cb8105e2fd1138327d6ea5edf44c9939c6693962
Heax2137 <dominikbomba12@gmail.com>
2025-03-01 05:26:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const songs = [
+    "https://github.com/Heax2137/Radyjko/blob/0f90105b13e8800aeafd5f40f16377121706027d/Radio%20Chlew%20-%20Alie%20Olie.mp3",
+    "https://github.com/twoje-konto/moje-radio/blob/main/piosenka2.mp3?raw=true",
+    "https://github.com/twoje-konto/moje-radio/blob/main/piosenka3.mp3?raw=true"
+];
+
+const player = document.getElementById("player");
+
+function nextSong() {
+    const randomIndex = Math.floor(Math.random() * songs.length);
+    player.src = songs[randomIndex];
+    player.play();
+}
+
+// Automatyczna zmiana po zakończeniu utworu
+player.addEventListener("ended", nextSong);
+
+// Wczytaj pierwszą piosenkę
+nextSong();
